bc343cd197c6b0f7ac94a7ab32c2d52c6a960c01
dalongxhxlr <dalongxhxlr@gmail.com>
2025-12-25 11:28:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+set -e
 
 export TZ=Asia/Shanghai
-# Fly.io 会自动注入 $PORT 环境变量
 export PORT=${PORT:-8080}
 
 echo "Fly.io PORT is $PORT"
 echo "server mode"
 
-exec /usr/local/bin/hysteria server \
+/usr/local/bin/hysteria server \
   --config /etc/hysteria/config.yaml
